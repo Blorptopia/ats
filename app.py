cc95dd3c51e5aafe3acd3723eaff0b730a185b1a
Blorptopia <blorptopia@example.com>
2026-06-08 18:33:03 +0200

    feat: minimal ats

---
+++ app.py
@@ -0,0 +1,317 @@
+import typing
+import asyncpg
+from fastapi import Depends, FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from contextlib import asynccontextmanager
+import tomllib
+from urllib.parse import urlsplit
+from jinja2.ext import DebugExtension
+from pydantic import BaseModel
+import re
+from uuid import UUID
+
+class ConfigSchema(BaseModel):
+    database_url: str
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> typing.AsyncGenerator[None]:
+    with open("config.toml", "rb") as f:
+        raw_config = tomllib.load(f)
+    config = ConfigSchema.model_validate(raw_config)
+    pool = await asyncpg.connect(config.database_url)
+    app.state["postgres"] = pool
+    yield
+
+async def get_db_pool() -> typing.AsyncGenerator[asyncpg.Pool]:
+    raw_pool = app.state["postgres"]
+    pool = typing.cast(asyncpg.Pool, raw_pool)
+    yield pool
+
+
+app = FastAPI(lifespan=lifespan)
+templates = Jinja2Templates("templates")
+templates.env.add_extension(DebugExtension)
+app.mount("/static", StaticFiles(directory="static"))
+
+FINN_URL_REGEX = re.compile("https://(?:www\\.)?finn.no/job/ad/(\\d+)")
+ARBEIDSPLASSEN_URL_REGEX = re.compile("https://arbeidsplassen.nav.no/stillinger/stilling/([\\w\\d\\-_]+)")
+
+@app.get("/")
+async def render_index(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(request, "index.jinja")
+
+@app.get("/applications")
+async def render_applications(
+    request: Request,
+    pool: typing.Annotated[asyncpg.Pool, Depends(get_db_pool)],
+    company_id: int | None = None,
+) -> HTMLResponse:
+    if company_id is None:
+        applications = await pool.fetch(
+            """
+            select
+                applications.id,
+                applied_at,
+                finn_ad_id,
+                arbeidsplassen_post_id,
+                application_platform_url,
+                status,
+                (
+                    select count(*) from interviews 
+                    where
+                        application_id = applications.id
+                ) as interview_count,
+                company_id,
+                companies.name as company_name
+            from applications
+            left join companies on companies.id = applications.company_id
+            order by applications.id desc
+            """
+        )
+    else:
+        applications = await pool.fetch(
+            """
+            select
+                applications.id,
+                applied_at,
+                finn_ad_id,
+                arbeidsplassen_post_id,
+                application_platform_url,
+                status,
+                (
+                    select count(*) from interviews 
+                    where
+                        application_id = applications.id
+                ) as interview_count,
+                company_id,
+                companies.name as company_name
+            from applications
+            left join companies on companies.id = applications.company_id
+            where
+                company_id = $1
+            order by applications.id desc
+            """,
+            company_id
+        )
+    return templates.TemplateResponse(
+        request,
+        "applications/list.jinja",
+        context={
+            "active_href": "/",
+            "applications": applications
+        }
+    )
+@app.post("/applications/{application_id}")
+async def update_application(
+    application_id: int,
+    status: typing.Annotated[typing.Literal["pending", "rejected", "offer"], Form()],
+    pool: typing.Annotated[asyncpg.Pool, Depends(get_db_pool)],
+) -> RedirectResponse:
+    await pool.execute(
+        """
+        update applications
+        set
+            status = $1
+        where
+            id = $2
+        """,
+        status,
+        application_id
+    )
+    return RedirectResponse(f"/applications/{application_id}", status_code=302)
+@app.get("/applications/find", response_model=None)
+async def find_application_by_id_or_url(
+    id_or_url: str,
+    request: Request,
+    pool: typing.Annotated[asyncpg.Pool, Depends(get_db_pool)]
+) -> HTMLResponse | RedirectResponse:
+    if (parsed := FINN_URL_REGEX.search(id_or_url)) is not None:
+        finn_ad_id = int(parsed.group(1))
+        application = await pool.fetchrow(
+            """
+                select
+                    id
+                from applications
+                where
+                    finn_ad_id = $1
+            """,
+            finn_ad_id
+        )
+        if application is not None:
+            return RedirectResponse(f"/applications/{application["id"]}")
+        else:
+            return templates.TemplateResponse(
+                request,
+                "applications/create.jinja",
+                context={
+                    "finn_ad_id": finn_ad_id
+                }
+            )
+    if (parsed := ARBEIDSPLASSEN_URL_REGEX.search(id_or_url)) is not None:
+        arbeidsplassen_post_id = parsed.group(1)
+        application = await pool.fetchrow(
+            """
+                select
+                    id
+                from applications
+                where
+                    arbeidsplassen_post_id = $1
+            """,
+            arbeidsplassen_post_id
+        )
+        if application is not None:
+            return RedirectResponse(f"/applications/{application["id"]}")
+        else:
+            return templates.TemplateResponse(
+                request,
+                "applications/create.jinja",
+                context={
+                    "arbeidsplassen_post_id": arbeidsplassen_post_id
+                }
+            )
+    try:
+        finn_ad_id = int(id_or_url)
+    except ValueError:
+        finn_ad_id = None
+    if finn_ad_id is not None:
+        application = await pool.fetchrow(
+            """
+                select
+                    id
+                from applications
+                where
+                    finn_ad_id = $1
+            """,
+            finn_ad_id
+        )
+        if application is not None:
+            return RedirectResponse(f"/applications/{application["id"]}")
+        else:
+            return templates.TemplateResponse(
+                request,
+                "applications/create.jinja",
+                context={
+                    "finn_ad_id": finn_ad_id
+                }
+            )
+    try:
+        arbeidsplassen_post_id = UUID(id_or_url)
+    except ValueError:
+        arbeidsplassen_post_id = None
+    if finn_ad_id is not None:
+        application = await pool.fetchrow(
+            """
+                select
+                    id
+                from applications
+                where
+                    arbeidsplassen_post_id = $1
+            """,
+            arbeidsplassen_post_id
+        )
+        if application is not None:
+            return RedirectResponse(f"/applications/{application["id"]}")
+        else:
+            return templates.TemplateResponse(
+                request,
+                "applications/create.jinja",
+                context={
+                    "arbeidsplassen_post_id": arbeidsplassen_post_id
+                }
+            )
+
+    application = await pool.fetchrow(
+        """
+            select
+                id
+            from applications
+            where
+                application_platform_url = $1
+        """,
+        id_or_url
+    )
+    if application is not None:
+        return RedirectResponse(f"/applications/{application["id"]}")
+    else:
+        return templates.TemplateResponse(
+            request,
+            "applications/create.jinja",
+            context={
+                "application_platform_url": arbeidsplassen_post_id
+            }
+        )
+@app.get("/applications/{application_id}")
+async def render_application(
+        application_id: int,
+        request: Request,
+        pool: typing.Annotated[asyncpg.Pool, Depends(get_db_pool)]
+) -> HTMLResponse:
+    application = await pool.fetchrow(
+        """
+        select
+            applications.id,
+            finn_ad_id,
+            arbeidsplassen_post_id,
+            application_platform_url,
+            status,
+            companies.id as company_id,
+            companies.name as company_name
+        from applications
+        left join companies on companies.id = applications.company_id
+        where
+            applications.id = $1
+        """,
+        application_id
+    )
+    interviews = await pool.fetch(
+        """
+        select
+            id,
+            date
+        from interviews
+        where
+            application_id = $1
+        """,
+        application_id
+    )
+    return templates.TemplateResponse(
+        request,
+        "applications/view.jinja",
+        context={
+            "active_href": f"/applications/{application_id}",
+            "application": application,
+            "interviews": interviews,
+            "get_hostname": get_hostname
+        }
+    )
+
+def get_hostname(url: str) -> str:
+    split = urlsplit(url)
+    return split.hostname or "unknown"
+
+@app.get("/companies")
+async def render_companies(request: Request, pool: typing.Annotated[asyncpg.Pool, Depends(get_db_pool)]) -> HTMLResponse:
+    companies = await pool.fetch(
+        """
+        select
+            id,
+            name,
+            (
+                select count(*) from applications
+                where
+                    company_id = companies.id
+            ) as application_count
+        from companies
+        order by id desc
+        """
+    )
+    return templates.TemplateResponse(
+        request,
+        "companies/list.jinja",
+        context={
+            "companies": companies,
+            "active_href": "/companies"
+        }
+    )
